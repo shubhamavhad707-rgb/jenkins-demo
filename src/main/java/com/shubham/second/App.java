462ad22626a98ee2dad98aa8133be0b45710e7b7
Shubhamavhad707 <Shubhamavhad707@gmail.com>
2026-04-27 07:28:35 +0530

    Fixed Jenkins pipeline

--- src/main/java/com/shubham/second/App.java
+++ src/main/java/com/shubham/second/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins Auti build Pipeline!" );
+        System.out.println( "Hello Jenkins Auto build Pipeline!" );
     }
 }

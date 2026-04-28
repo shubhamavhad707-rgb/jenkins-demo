ad6164c32b4d4b89dc4ce66286a2a1d6819e61aa
Shubhamavhad707 <Shubhamavhad707@gmail.com>
2026-04-28 07:53:56 +0530

    commiting java file

--- src/main/java/com/shubham/second/App.java
+++ src/main/java/com/shubham/second/App.java
@@ -10,18 +10,4 @@ public class App
     {
         System.out.println( "Hello Jenkins Auto build Pipeline!" );
     }
-
-    post {
-           success {
-            echo 'Build Successful'
-            archiveArtifacts artifacts: 'target/*.jar'
-        }
-        failure {
-            echo 'Build Failed'
-        }
-        always {
-            echo 'Pipeline Finished'
-        }
     }
-    }
-}

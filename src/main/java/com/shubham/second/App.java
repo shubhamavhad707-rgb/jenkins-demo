476bf3c95e27de6d0b8aca702498005db4f3291f
Shubhamavhad707 <Shubhamavhad707@gmail.com>
2026-04-28 07:27:15 +0530

    Jenkins artifacts

--- src/main/java/com/shubham/second/App.java
+++ src/main/java/com/shubham/second/App.java
@@ -10,4 +10,18 @@ public class App
     {
         System.out.println( "Hello Jenkins Auto build Pipeline!" );
     }
+
+    post {
+           success {
+            echo 'Build Successful'
+            archiveArtifacts artifacts: 'target/*.jar'
+        }
+        failure {
+            echo 'Build Failed'
+        }
+        always {
+            echo 'Pipeline Finished'
+        }
+    }
+    }
 }
